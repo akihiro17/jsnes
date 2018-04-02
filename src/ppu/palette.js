396af8aa907e86885214dc69ad64307bcba76970
akihiro17 <coolwizard11@gmail.com>
2018-04-03 00:16:07 +0900

    feat: パレットのミラーを実装

--- src/ppu/palette.js
+++ src/ppu/palette.js
@@ -10,7 +10,11 @@ export default class Palette {
     }
 
     read(): Uint8Array {
-        return this.palette;
+        return this.palette.map((v: Byte, i: number) => {
+            if (this.isSpriteMirror(i)) return this.palette[i - 0x10];
+            if (this.isBackgroundMirror(i)) return this.palette[0x00];
+            return v;
+        })
     }
 
     write(address: Word, data: Byte) {
@@ -25,5 +29,10 @@ export default class Palette {
     isSpriteMirror(address: Byte) {
         // ただし、0x3F10,0x3F14,0x3F18,0x3F1Cは0x3F00,0x3F04,0x3F08,0x3F0Cのミラーとなっている
         return (address === 0x10) || (address === 0x14) || (address === 0x18) || (address === 0x1C);
+    }
+
+    isBackgroundMirror(address: Byte): boolean {
+        // また、0x3F04,0x3F08,0x3F0Cはユニークな値を持つんですが、PPUには使用されず、0x3F00の値が適用されます
+        return (address === 0x04) || (address === 0x08) || (address === 0x0C);
     }
 }
